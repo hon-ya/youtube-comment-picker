dfa03c578196bd8fcd5bd3042fcbcc5759564a97
hon_ya <spaceodyssey2008@gmail.com>
2019-08-06 09:54:52 +0900

    モデレーターのコメントをピックアップ対象とする機能を修正
    * バッジによる判定では正しく動作しないことがある
    * author-type を見ることでより簡単に判定できる

--- contentScript.js
+++ contentScript.js
@@ -50,20 +50,7 @@ CommentAccessor.prototype.getIconUrl = function() {
 }
 
 CommentAccessor.prototype.isModerator = function() {
-  const badges = this.node.querySelector("#chat-badges");
-  if (badges === null) {
-    return false;
-  }
-
-  // モデレーターアイコン
-  const moderatorIcon = "M9.64589146,7.05569719 C9.83346524,6.562372 9.93617022,6.02722257 9.93617022,5.46808511 C9.93617022,3.00042984 7.93574038,1 5.46808511,1 C4.90894765,1 4.37379823,1.10270499 3.88047304,1.29027875 L6.95744681,4.36725249 L4.36725255,6.95744681 L1.29027875,3.88047305 C1.10270498,4.37379824 1,4.90894766 1,5.46808511 C1,7.93574038 3.00042984,9.93617022 5.46808511,9.93617022 C6.02722256,9.93617022 6.56237198,9.83346524 7.05569716,9.64589147 L12.4098057,15 L15,12.4098057 L9.64589146,7.05569719 Z";
-
-  const found =Array.prototype.find.call(this.node.querySelectorAll("path"), path => {
-    // バッジアイコンがモデレーターであるかどうか
-    return path.attributes["d"].value === moderatorIcon;
-  });
-
-  return found;
+  return this.node.attributes["author-type"].value === "moderator";
 }
 
 const init = async() => {
